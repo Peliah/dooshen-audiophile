119140ffdfe1847368fc98854c86a2301d54c7f2
Peliah <pelepoupa@gmail.com>
2025-11-04 03:56:52 +0100

    feat: Added the hero page with its assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,9 @@
-import { Button } from "@/components/ui/button";
+import Hero from "@/components/landing-page/hero";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <div className="flex gap-4 my-10">
-        <Button>Hello World</Button>
-        <Button variant={"outline"}>Hello World</Button>
-        <Button variant={"secondary"}>Hello World</Button>
-      </div>
-
-      <h1>Lorem ipsum dolor sit amet.</h1>
-      <h2>Lorem ipsum dolor sit amet.</h2>
-      <h3>Lorem ipsum dolor sit amet.</h3>
-      <h4>Lorem ipsum dolor sit amet.</h4>
-      <h5>Lorem ipsum dolor sit amet.</h5>
-      <h6>Lorem ipsum dolor sit amet.</h6>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore, eos.
-      </p>
-      <p className="brand-overline">
-        Lorem ipsum dolor sit amet, consectetur adipisicing.
-      </p>
-      <p className="subtitle">
-        Lorem ipsum dolor sit amet consectetur adipisicing.
-      </p>
-    </div>
+    <main>
+      <Hero/>
+    </main>
   );
 }
---
+++ components/landing-page/hero.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button } from "../ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+const Hero = () => {
+  return (
+    <section className="bg-[#141414] max-md:min-h-[729px] md:min-h-[800px] flex items-center relative">
+      <div className="md:hidden absolute inset-0 bg-[url(/assets/images/hero-mobile.svg)] bg-no-repeat bg-center" />
+      <div className="absolute inset-0 w-full h-full">
+        <Image
+          src="/assets/images/image-hero.jpg"
+          alt="hero image desktop"
+          fill
+          priority
+          className="hidden lg:block w-full h-full object-cover"
+        />
+      </div>
+      <div className="max-w-[1110px] h-full w-full mx-auto md:px-6 text-white flex justify-between items-center gap-2">
+        <div className="md:max-w-[398px] z-10 max-md:px-6 max-md:flex flex-col max-md:items-center max-md:text-center">
+          <p className="brand-overline opacity-50">NEW PRODUCT</p>
+          <h1 className="my-6">XX99 Mark II Headphones</h1>
+          <p className="max-w-[350px] mb-10 opacity-75 font-medium">
+            Experience natural, lifelike audio and exceptional build quality
+            made for the passionate music enthusiast.
+          </p>
+          <Link href={"/headphones/xx99-mark-two"}>
+            <Button className="">SEE PRODUCT</Button>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
