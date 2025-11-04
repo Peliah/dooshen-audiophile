c6719cb7afcfed6e9502cf2f88e676f80cb50b6b
Peliah <pelepoupa@gmail.com>
2025-11-04 04:08:03 +0100

    feat: integrated product to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/landing-page/hero";
+import Products from "@/components/landing-page/products";
 
 export default function Home() {
   return (
     <main>
       <Hero/>
+      <Products/>
     </main>
   );
 }
--- components/landing-page/products.tsx
+++ components/landing-page/products.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Button } from "../ui/button";
-
 import Image from "next/image";
 import Link from "next/link";
 import QuickProducts from "./quick-products";
